9730ac457d80703c35629e1dee9d95250397cc5c
egesevinc <egesevinc@windowslive.com>
2024-10-31 22:20:27 +0300

    first api call

---
+++ src/api.tsx
@@ -0,0 +1,31 @@
+import axios from "axios"
+import { CompanySearch } from "./company"
+
+
+
+interface searchResponse {
+
+    data:CompanySearch[];
+
+
+}
+export const searchCompanies = async(query : string) => {
+    try {
+        const data = await axios.get<searchResponse>(
+            `https://financialmodelingprep.com/api/v3/search-ticker?query=AA&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+        );
+        
+        return data;
+
+    } catch(error){
+        if(axios.isAxiosError(error)){
+            console.log("error message: ", error.message);
+            return error.message;
+        }
+        else {
+            console.log("unexpected error: ", error);
+            return "an unexpected error has ocurred.";
+        }
+    }
+
+}
