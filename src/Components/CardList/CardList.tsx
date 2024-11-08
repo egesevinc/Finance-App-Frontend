7874ced419dd7462860ffe1666c099eadbb17cdf
egesevinc <egesevinc@windowslive.com>
2024-11-09 00:49:42 +0300

    Forms added

--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import Card from "../Card/Card";
 import { CompanySearch } from "../../company";
 import { v4 as uuidv4 } from "uuid";
 
 interface Props {
   searchResult: CompanySearch[];
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const CardList: React.FC<Props> = ({ searchResult }: Props): JSX.Element => {
+const CardList: React.FC<Props> = ({ searchResult, onPortfolioCreate }: Props): JSX.Element => {
   return (
     <>
       {searchResult.length > 0 ? (
         searchResult.map((result) => {
           return (
-            <Card id={result.symbol} key={uuidv4()} searchResult={result} />
+            <Card id={result.symbol} key={uuidv4()} searchResult={result} onPortfolioCreate={onPortfolioCreate}/>
           );
         })
       ) : (
--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,18 +1,22 @@
 import React, { ChangeEvent, useState, SyntheticEvent } from "react";
 
-interface Props  {
+interface Props {
   search: string | undefined;
-  handleChange : (e: ChangeEvent<HTMLInputElement>) => void;
-  onClick: (e : SyntheticEvent) => void;
-};
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (e: SyntheticEvent) => void;
+}
 
-const Search: React.FC<Props> = ({search,handleChange,onClick}: Props): JSX.Element => {
-
+const Search: React.FC<Props> = ({
+  search,
+  handleSearchChange,
+  onSearchSubmit,
+}: Props): JSX.Element => {
   return (
-    <div>
-      <input value={search} onChange={(e) => handleChange(e)}/>
-      <button onClick={(e) => onClick(e)}/>
-    </div>
+    <>
+      <form action="" onSubmit={onSearchSubmit}>
+        <input value={search} onChange={handleSearchChange} />
+      </form>
+    </>
   );
 };
 
