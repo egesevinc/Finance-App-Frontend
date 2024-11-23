78a000e96eceecb69b5d5f32ca5f3bb592daa25c
egesevinc <egesevinc@windowslive.com>
2024-11-23 22:20:28 +0300

    Dashboard

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -3,16 +3,25 @@ import App from "../App";
 import HomePage from "../Pages/HomePage/HomePage";
 import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
+import path from "path";
+import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
+import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 
 export const router = createBrowserRouter([
-    { 
-        path: "/",
-        element: <App />,
-        children: [
-            {path: "", element: <HomePage/>},
-            {path: "search", element: <SearchPage/>},
-            {path: "company/:ticker", element: <CompanyPage/>}
-        ]
-    }
-
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "search", element: <SearchPage /> },
+      {
+        path: "company/:ticker",
+        element: <CompanyPage />,
+        children: [ 
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+        ],
+      },
+    ],
+  },
 ]);
