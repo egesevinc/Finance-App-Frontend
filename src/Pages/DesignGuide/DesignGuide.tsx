6461fe29f540c6b26a4fc5abf7e1512a92d3e2cc
egesevinc <egesevinc@windowslive.com>
2024-12-23 00:11:19 +0300

    List added

--- src/Pages/DesignGuide/DesignGuide.tsx
+++ src/Pages/DesignGuide/DesignGuide.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Table from "../../Components/Table/Table";
 import RatioList from "../../Components/RatioList/RatioList";
 import { TestDataCompany } from "../../Components/Table/testData";
+import { CompanyKeyMetrics } from "../../company";
 
 type Props = {};
 
@@ -21,8 +22,8 @@ const DesignGuide = (props: Props) => {
         Design guide- This is the design guide for Fin Shark. These are reuable
         components of the app with brief instructions on how to use them.
       </h1>
-      <RatioList />
-      <Table />
+      <RatioList config={tableConfig} data={data} />
+      <Table config={tableConfig} data={data} />
       <h3>
         Table - Table takes in a configuration object and company data as
         params. Use the config to style your table.
